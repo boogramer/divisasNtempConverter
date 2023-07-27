198605e17feb97abd9a07f0d2d5895852d25a49e
boogramer <dvelaflores84@gmail.com>
2023-07-27 14:12:25 -0500

    se añadieron las monedas al choice panel

--- src/main/java/com/converter/divisasntempconverter/HelloController.java
+++ src/main/java/com/converter/divisasntempconverter/HelloController.java
@@ -1,14 +1,53 @@
 package com.converter.divisasntempconverter;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private Tab bienvenido_tab;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Tab convDiv_tab;
+
+    @FXML
+    private Tab convTemp_tab;
+
+    @FXML
+    private TabPane dashboard_tp;
+
+    @FXML
+    private TextField inputMoneda_txtfield;
+
+    @FXML
+    private ImageView intercambiarMoneda_img;
+
+    @FXML
+    private StackPane mainDashboard_Sp;
+
+    @FXML
+    private ChoiceBox<String> monedaDestino_choicebx;
+
+    @FXML
+    private ChoiceBox<String> monedaOr_choicebx;
+
+    @FXML
+    private TextField outputMoneda_txtfield;
+
+    private final String[] moneda = {"PEN S/. - Nuevo Sol Peruano", "USD $ - Dólar Estadounidense", "EUR € - Euro", "JYP ¥ - Yen Japonés", "KRW ₩ - Won Surcoreano", "GBP £ - Libra Esterlina"};
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        monedaOr_choicebx.getItems().addAll(moneda);
+        monedaOr_choicebx.setValue("PEN S/. - Nuevo Sol Peruano");
+        monedaDestino_choicebx.getItems().addAll(moneda);
+        monedaDestino_choicebx.setValue("USD $ - Dólar Estadounidense");
+
     }
 }
