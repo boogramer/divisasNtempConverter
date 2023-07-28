d579d7f272a18792272cd470240b983b5f2742b2
boogramer <dvelaflores84@gmail.com>
2023-07-28 10:43:17 -0500

    Se creo el paquete y la clase para la conexion con la api

---
+++ src/main/java/com/converter/divisasntempconverter/conectionAPI/conectionToAPI.java
@@ -0,0 +1,11 @@
+package com.converter.divisasntempconverter.conectionAPI;
+
+public class conectionToAPI {
+    private  final String url;
+    private final String apiKey;
+
+    public conectionToAPI(String url, String apiKey) {
+        this.url = url;
+        this.apiKey = apiKey;
+    }
+}
